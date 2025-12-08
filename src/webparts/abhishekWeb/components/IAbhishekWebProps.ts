e7e3715ebd2bb30533a06845408daf0f567546e5
abhishekvadher1104 <abhishekvadher1104@gmail.com>
2025-12-08 12:43:35 +0530

    changes in my web

---
+++ src/webparts/abhishekWeb/components/IAbhishekWebProps.ts
@@ -0,0 +1,7 @@
+export interface IAbhishekWebProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/abhishekWeb/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IAbhishekWebWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'AbhishekWebWebPartStrings' {
+  const strings: IAbhishekWebWebPartStrings;
+  export = strings;
+}
