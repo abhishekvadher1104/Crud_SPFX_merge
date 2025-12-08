c23b8f958fcb7f5f3bbd9b2436bd3e6a47c34479
abhishekvadher1104 <abhishekvadher1104@gmail.com>
2025-12-08 15:26:23 +0530

    added abhi web part

---
+++ src/webparts/testing/components/ITestingProps.ts
@@ -0,0 +1,7 @@
+export interface ITestingProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/testing/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITestingWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TestingWebPartStrings' {
+  const strings: ITestingWebPartStrings;
+  export = strings;
+}
